13af65110b7265b5a0225ed78d1adf3f199b077a
tuatang <157876640+tuatang55@users.noreply.github.com>
2024-09-23 16:23:17 +0700

    added example

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .destinations import get_countries_by_continent
 from .foods import get_foods_by_selected_countries
+from example.views import index
 
 urlpatterns = [
     path('countries/', get_countries_by_continent, name='countries_by_continent'),
-    path('foods/', get_foods_by_selected_countries, name='get_foods')
+    path('foods/', get_foods_by_selected_countries, name='get_foods'),
+    path('', index),
 ]
--- example/views.py
+++ example/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
-
-# Create your views here.
 # example/views.py
 from datetime import datetime
+
 from django.http import HttpResponse
+
 def index(request):
     now = datetime.now()
     html = f'''
